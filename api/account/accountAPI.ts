ffe782ff76f151eb883c1c9ac0aacc363884a214
Pavel <eropkinp@gmail.com>
2023-11-04 01:05:08 +0300

    Added new method of API and refactoring

--- api/account/accountAPI.ts
+++ api/account/accountAPI.ts
@@ -6,34 +6,44 @@ import {expect} from "@playwright/test";
 export default class AccountAPI extends AccountBaseApi {
 
     private userPath = "/User";
+    private authorizedPath = "Authorized";
     private generateTokenPath = "/GenerateToken";
 
     constructor(request) {
         super(request);
     }
-    public async addUser(User: User): Promise<APIResponse> {
-        let response = await this.request.post(this.basePath + this.userPath, {
-            data: {
-                "userName": User.username,
-                "password": User.password
-            }
-        });
+    public async addUser(user: User): Promise<APIResponse> {
+        let response = await this.post(user, this.basePath + this.userPath);
 
         expect(response.status(), await response.text()).toBe(201);
         return response;
     }
 
-    public async generateToken(User: User): Promise<APIResponse> {
-        let response = await this.request.post(this.basePath + this.generateTokenPath, {
-            data: {
-                "userName": User.username,
-                "password": User.password
+    public async generateToken(user: User): Promise<APIResponse> {
+        let response = await this.post(user, this.basePath + this.generateTokenPath);
+
+        expect(response.status(), await response.text()).toBe(200);
+        return response;
+    };
+
+    public async makeAuthorized(user: User, path: string): Promise<APIResponse> {
+        let response = await this.post(user, this.basePath + this.authorizedPath);
+
+        expect(response.status(), await response.text()).toBe(200);
+        return response;
+    }
+
+    public async delegteUser(userID: string, token: string): Promise<APIResponse> {
+        let response: APIResponse = await this.request.delete(this.basePath + this.userPath + "/" + userID, {
+            headers: {
+                "accept": "application/json",
+                "Authorization": "Bearer " + token,
             }
         });
 
         expect(response.status(), await response.text()).toBe(200);
         return response;
-    };
+    }
 
     public async getUser(userID: string, token: string): Promise<APIResponse> {
         let response: APIResponse = await this.request.get(this.basePath + this.userPath + "/" + userID, {
--- api/account/accountBaseAPI.ts
+++ api/account/accountBaseAPI.ts
@@ -1,9 +1,22 @@
+import {APIResponse} from "playwright";
+import User from "../../data/model/user";
+
 export default class AccountBaseAPI {
 
     protected basePath = "https://bookstore.toolsqa.com/Account/v1";
     protected request;
-
     constructor(request) {
         this.request = request;
+    }
+
+    public async post(user: User, path: string): Promise<APIResponse> {
+        let response = await this.request.post(path, {
+            data: {
+                "userName": user.username,
+                "password": user.password
+            }
+        });
+
+        return response;
     }
 }
